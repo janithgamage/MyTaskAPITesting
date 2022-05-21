7f79ad842455dc8e9201ce695c74da947e484803
Janith <janithchathuranga71@gmail.com>
2022-05-21 10:37:25 +0530

    initial commit

---
+++ MyTaskToday/Controllers/TaskApiController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTaskToday.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskApiController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Task()
+        {
+            string[] abc=new string[] { "task1", "task2", "task3" };
+
+            return abc;
+
+
+        }
+
+       
+
+        [HttpPost]
+        public IActionResult NewTaske()
+        {
+            
+            return Ok();
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTaske()
+        {
+
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTaske()
+        {
+
+            return Ok();
+
+        }
+
+    }
+
+
+}
